96df7e77e909294ce1a0476f9ab5bfbc5b36455f
Aidan Follestad <drummer.aidan@gmail.com>
2016-02-11 12:15:28 -0600

    Update

--- src/main/java/com/afollestad/polarupgradetool/jfx/WindowScene.java
+++ src/main/java/com/afollestad/polarupgradetool/jfx/WindowScene.java
@@ -301,11 +301,7 @@ public class WindowScene {
             System.exit(0);
         });
         alert.setResizable(true);
-        Optional<ButtonType> result = alert.showAndWait();
-        if (result.isPresent() && result.get() == ButtonType.OK) {
-            Platform.exit();
-            System.exit(0);
-        }
+        alert.showAndWait();
     }
 
     public void showSecurityInfoDialog(String path) {
